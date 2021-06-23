8aaf51649df5c47a813832abc538ed15b01a1697
mcreek <mohan.creek@gmail.com>
2021-06-23 15:03:30 +0000

    first checks

---
+++ 1001-git-checks.sh
@@ -0,0 +1,3 @@
+sudo git clone https://github.com/ponmoh/unix-flavor.git
+cd unix-flavor/
+
---
+++ git-config.sh
@@ -0,0 +1,3 @@
+git config --global user.name "mcreek"
+git config --global user.email "mohan.creek@gmail.com"
+
